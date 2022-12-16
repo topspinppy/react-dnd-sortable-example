9976e6fc872ac12f12c08c415d98a3125f701950
Paranat Klinhom <paranat@mercular.com>
2022-12-16 17:51:26 +0700

    feat: add Cancel on Drop outside

--- src/TaskCard.tsx
+++ src/TaskCard.tsx
@@ -1,80 +1,110 @@
-import { Box } from "@chakra-ui/react"
-import { useRef } from "react"
-import { useDrag, useDrop, XYCoord } from "react-dnd"
-import itemTypes from "./utils/itemType"
+import { Box } from "@chakra-ui/react";
+import { useRef } from "react";
+import { useDrag, useDrop, XYCoord } from "react-dnd";
+import itemTypes from "./utils/itemType";
 
 interface ITaskCard {
-  details: string,
-  id: string | number,
-  index: number
-  moveTask: (dragIndex: number, hoverIndex: number) => void
+  details: string;
+  id: string | number;
+  index: number;
+  moveTask: (dragIndex: number, hoverIndex: number) => void;
+  onClick?(details: string, id: number | string, index: number): void;
+  getOriginalIndex: (id: number | string) => { index: number }
 }
 
 interface DragItem {
-  index: number
-  id: string
-  type: string
+  index: number;
+  id: string;
+  type: string;
+}
+
+interface Item {
+  id: number;
+  index: number;
+  originalIndex: number;
 }
 
 const TaskCard: React.FC<ITaskCard> = (props) => {
-  const {details, id, index, moveTask, onClick} = props
-  const ref = useRef<HTMLDivElement>(null)
-
-  const [{ isDragging }, drag] = useDrag<any, any, any>({
-    type: itemTypes.CARD,
-    item: () => {
-      return { id, index }
+  const { details, id, index, moveTask, onClick, getOriginalIndex } = props;
+  const ref = useRef<HTMLDivElement>(null);
+  const originalIndex = getOriginalIndex(id).index
+  const [{ isDragging }, drag] = useDrag<any, any, any>(
+    {
+      type: itemTypes.CARD,
+      item: () => {
+        return { id, index, originalIndex };
+      },
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+      }),
+      end(draggedItem: Item, monitor) {
+        const { index, originalIndex } = draggedItem;
+        const didDrop = monitor.didDrop();
+        if (!didDrop) {
+          console.log("drop outside !! ", id , index, originalIndex)
+          moveTask(index, originalIndex)
+        }
+      },
     },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging()
-    }),
-  })
+    [id, index, moveTask]
+  );
 
-  const [, drop] = useDrop<DragItem, void, { handlerId: any }>({
-    accept: itemTypes.CARD,
-    collect(monitor) {
-      return {
-        handlerId: monitor.getHandlerId()
-      }
+  const [, drop] = useDrop<DragItem, void, { handlerId: any }>(
+    {
+      accept: itemTypes.CARD,
+      collect(monitor) {
+        return {
+          handlerId: monitor.getHandlerId(),
+        };
+      },
+      hover(item: DragItem, monitor) {
+        if (!ref.current) {
+          return;
+        }
+        const dragIndex = item.index;
+        const hoverIndex = index;
+        if (dragIndex === hoverIndex) {
+          return;
+        }
+        const hoverBoundingRect = ref.current?.getBoundingClientRect();
+        const hoverMiddleY =
+          (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+        const clientOffset = monitor.getClientOffset();
+        const hoverClientY =
+          (clientOffset as XYCoord).y - hoverBoundingRect.top;
+
+        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+          return;
+        }
+
+        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+          return;
+        }
+
+        moveTask(dragIndex, hoverIndex);
+        item.index = hoverIndex;
+      },
+      drop(item, monitor) {
+        console.log("DROP: ", item);
+        console.log("DROP: ", monitor.canDrop());
+      },
     },
-    hover(item: DragItem, monitor) {
-      if (!ref.current) {
-        return;
-      }
-      const dragIndex = item.index
-      const hoverIndex = index
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-      const hoverBoundingRect = ref.current?.getBoundingClientRect()
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
-      const clientOffset = monitor.getClientOffset()
-      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
+    [moveTask]
+  );
 
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return
-      }
-
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return
-      }
-
-      moveTask(dragIndex, hoverIndex);
-      item.index = hoverIndex
-    },
-    drop(item, monitor) {
-      console.log("DROP: ", item);
-      console.log("DROP: ", monitor.canDrop());
-    }
-  })
-
-  drag(drop(ref))
+  drag(drop(ref));
   return (
-    <Box ref={ref} opacity={isDragging ? 0.5 : 1} bg="yellow.300" p="3" boxShadow='sm'>
+    <Box
+      ref={ref}
+      opacity={isDragging ? 0.5 : 1}
+      bg="yellow.300"
+      p="3"
+      boxShadow="sm"
+      onClick={() => onClick && onClick(details, id, index)}
+    >
       {details}
     </Box>
-  )
-}
+  );
+};
 
-export default TaskCard
+export default TaskCard;
--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,64 +1,69 @@
-import { Box, Grid, Stack, useColorMode, Text } from "@chakra-ui/react"
+import { Box, Grid, Stack, useColorMode, Text } from "@chakra-ui/react";
 import { useCallback, useState } from "react";
 import TaskCard from "./TaskCard";
 
-
 const Tasks: React.FC = () => {
   const { colorMode } = useColorMode();
-  const bg = { light: "teal.300", dark: "teal.600" }
+  const bg = { light: "teal.300", dark: "teal.600" };
   const [taskList, setTaskList] = useState([
     {
       id: 1,
-      attributeName: 'มีสาย/ไร้สาย',
+      attributeName: "มีสาย/ไร้สาย",
     },
     {
       id: 2,
-      attributeName: 'การเชื่อมต่อหูฟัง(Input)',
+      attributeName: "การเชื่อมต่อหูฟัง(Input)",
     },
     {
       id: 3,
-      attributeName: 'ไมโครโฟน',
+      attributeName: "ไมโครโฟน",
     },
     {
       id: 4,
-      attributeName: 'ระดับการกันน้ำ',
+      attributeName: "ระดับการกันน้ำ",
     },
     {
       id: 5,
-      attributeName: 'การเชื่อมต่อลำโพง',
+      attributeName: "การเชื่อมต่อลำโพง",
     },
     {
       id: 6,
-      attributeName: 'คุณสมบัติพิเศษ',
+      attributeName: "คุณสมบัติพิเศษ",
     },
     {
       id: 7,
-      attributeName: 'ขั้วชาร์จ',
+      attributeName: "ขั้วชาร์จ",
     },
     {
       id: 8,
-      attributeName: 'ประเภทเคส',
+      attributeName: "ประเภทเคส",
     },
     {
       id: 9,
-      attributeName: 'bit depth',
+      attributeName: "bit depth",
     },
-  ])
+  ]);
 
-  const handleMoveTask = useCallback((dragIndex: number, hoverIndex: number) => {
-    // console.log(dragIndex, hoverIndex)
-    // console.log('dragIndex (ของที่ถูกลาก) ->',taskList[dragIndex])
-    // console.log('hoverIndex (ปลายทางที่เอาไปวาง) ->',taskList[hoverIndex])
+  const getOriginalIndex = (id: number | string) => {
+    const card = taskList.filter((list) => list.id === +id)[0];
+    return {
+      card,
+      index: taskList.indexOf(card),
+    };
+  };
 
-    const dragData = taskList[dragIndex]
-    const hoverData = taskList[hoverIndex]
-    const newArray = [...taskList]
-    newArray[dragIndex] = hoverData
-    newArray[hoverIndex] = dragData;
+  const handleMoveTask = useCallback(
+    (dragIndex: number, hoverIndex: number) => {
+      const dragData = taskList[dragIndex];
+      const hoverData = taskList[hoverIndex];
+      const newArray = [...taskList];
+      newArray[dragIndex] = hoverData;
+      newArray[hoverIndex] = dragData;
 
-    console.log(newArray)
-    setTaskList(newArray)
-  }, [taskList])
+      setTaskList(newArray);
+    },
+    [taskList]
+  );
   return (
     <Grid gap={6} templateColumns="repeat(2,1fr)" w="80vw" h="93vh" p={5}>
       <Box bg={bg[colorMode]} rounded="md" p={3}>
@@ -69,13 +74,20 @@ const Tasks: React.FC = () => {
             </Text>
           </Box>
           {taskList.map((task, index) => (
-            <TaskCard details={task.attributeName} key={task.id} id={task.id} index={index} moveTask={handleMoveTask}/>
+            <TaskCard
+              details={task.attributeName}
+              key={task.id}
+              id={task.id}
+              index={index}
+              moveTask={handleMoveTask}
+              getOriginalIndex={getOriginalIndex}
+            />
           ))}
         </Stack>
       </Box>
       <Box></Box>
     </Grid>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
